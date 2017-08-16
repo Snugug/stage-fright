f45de34aa206166018e0b675219f9ded0a6942d9
Sam Richard <sam@snug.ug>
2017-08-16 12:03:25 -0500

    :new: Add key modifiers

--- src/js/lib/keys.js
+++ src/js/lib/keys.js
@@ -5,6 +5,70 @@ export default function(matrix, opts) {
   const arrowNav = opts.get('navigation.arrows', true);
   const remoteNav = opts.get('navigation.remote', true);
   const spaceNav = opts.get('navigation.spacebar', true);
+  const notes = opts.get('notes', true);
+
+  function arrowMove(e) {
+    // Left Arrow
+    if (e.keyCode === 37) {
+      return nav.left(matrix);
+    }
+
+    // Right Arrow
+    if (e.keyCode === 39) {
+      return nav.right(matrix);
+    }
+
+    // Up Arrow
+    if (e.keyCode === 38) {
+      return nav.previous(matrix);
+    }
+
+    // Down Arrow
+    if (e.keyCode === 40) {
+      return nav.next(matrix);
+    }
+  }
+
+  function spaceMove(e) {
+    // Shift + Spacebar
+    if (e.shiftKey === true && e.keyCode === 32) {
+      return nav.previous(matrix);
+    }
+
+    // Spacebar
+    if (e.keyCode === 32) {
+      return nav.next(matrix);
+    }
+  }
+
+  function notesOpen(e) {
+    // S
+    if (e.keyCode === 83) {
+      matrix.notes = openNotes(matrix);
+      return matrix;
+    }
+  }
+
+  function modifierScaffold(identifier, action, e) {
+    if (identifier === true) {
+      action(e);
+    }
+    else if (identifier === 'ctrl') {
+      if (e.ctrlKey) {
+        action(e);
+      }
+    }
+    else if (identifier === 'alt') {
+      if (e.altKey) {
+        action(e);
+      }
+    }
+    else if (identifier === 'meta') {
+      if (e.metaKey) {
+        action(e);
+      }
+    }
+  }
 
   document.addEventListener('keydown', (e) => {
     if (matrix.stage.hasAttribute('data-overlay')) {
@@ -14,27 +78,7 @@ export default function(matrix, opts) {
     //////////////////////////////
     // Arrow Navigation
     //////////////////////////////
-    if (arrowNav === true) {
-      // Left Arrow
-      if (e.keyCode === 37) {
-        return nav.left(matrix);
-      }
-
-      // Right Arrow
-      if (e.keyCode === 39) {
-        return nav.right(matrix);
-      }
-
-      // Up Arrow
-      if (e.keyCode === 38) {
-        return nav.previous(matrix);
-      }
-
-      // Down Arrow
-      if (e.keyCode === 40) {
-        return nav.next(matrix);
-      }
-    }
+    modifierScaffold(arrowNav, arrowMove, e);
 
     //////////////////////////////
     // Remote Navigation
@@ -54,23 +98,12 @@ export default function(matrix, opts) {
     //////////////////////////////
     // Spacebar Navigation
     //////////////////////////////
-    if (spaceNav === true) {
-      // Shift + Spacebar
-      if (e.shiftKey === true && e.keyCode === 32) {
-        return nav.previous(matrix);
-      }
+    modifierScaffold(spaceNav, spaceMove, e);
 
-      // Spacebar
-      if (e.keyCode === 32) {
-        return nav.next(matrix);
-      }
-    }
-
-    // S
-    if (e.keyCode === 83) {
-      matrix.notes = openNotes(matrix);
-      return matrix;
-    }
+    //////////////////////////////
+    // Activate Notes
+    //////////////////////////////
+    modifierScaffold(notes, notesOpen, e);
 
     return;
   });
