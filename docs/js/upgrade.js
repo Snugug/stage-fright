2261e7d106172455cc54ad6ce5571cff52f3a9fa
Sam Richard <snugug@google.com>
2019-07-03 12:22:22 +0800

    :memo: More docs compiling

--- docs/js/upgrade.js
+++ docs/js/upgrade.js
@@ -1,11 +1,11 @@
 import { loadImage, loadMedia } from './lazyload.js';
 
 var icons = {
-  download: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M26 15l-1.41-1.41L17 21.17V2h-2v19.17l-7.59-7.58L6 15l10 10 10-10z"/><path d="M26 24v4H6v-4H4v4a2 2 0 0 0 2 2h20a2 2 0 0 0 2-2v-4z"/></svg>',
-  presentation: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M15 10h2v8h-2zm5 4h2v4h-2zm-10-2h2v6h-2z"/><path d="M25 4h-8V2h-2v2H7a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h8v6h-4v2h10v-2h-4v-6h8a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 16H7V6h18z"/></svg>',
-  article: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M4 24h10v2H4zm0-6h10v2H4zm22-4H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h20a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2zM6 6v6h20V6zm20 22h-6a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h6a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2zm-6-8v6h6v-6z"/></svg>',
-  help: '<svg role="presentation" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><path d="M16 2a14 14 0 1 0 14 14A14 14 0 0 0 16 2zm0 26a12 12 0 1 1 12-12 12 12 0 0 1-12 12z"/><circle cx="16" cy="23.5" r="1.5"/><path d="M17 8h-1.5a4.49 4.49 0 0 0-4.5 4.5v.5h2v-.5a2.5 2.5 0 0 1 2.5-2.5H17a2.5 2.5 0 0 1 0 5h-2v4.5h2V17a4.5 4.5 0 0 0 0-9z"/></svg>',
-  speaker: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" style="transform: rotate(180deg)"><path d="M28 4H10a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2zm0 16H10V6h18z"/><path d="M16 26H4V16h2v-2H4a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-2h-2z"/></svg>'
+  download: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M18.32 4.26A9.949 9.949 0 0 0 13 2.05v2.02c1.46.18 2.79.76 3.9 1.62l1.42-1.43zM19.93 11h2.02c-.2-2.01-1-3.84-2.21-5.32L18.31 7.1a7.941 7.941 0 0 1 1.62 3.9zm-1.62 5.9l1.43 1.43a9.981 9.981 0 0 0 2.21-5.32h-2.02a7.945 7.945 0 0 1-1.62 3.89zM13 19.93v2.02c2.01-.2 3.84-1 5.32-2.21l-1.43-1.43c-1.1.86-2.43 1.44-3.89 1.62zm2.59-9.34L13 13.17V7h-2v6.17l-2.59-2.59L7 12l5 5 5-5-1.41-1.41zM11 19.93v2.02c-5.05-.5-9-4.76-9-9.95 0-5.19 3.95-9.45 9-9.95v2.02C7.05 4.56 4 7.92 4 12s3.05 7.44 7 7.93z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>',
+  presentation: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M2.5 6.5H4v11H2.5zM20 6.5h1.5v11H20zM22.5 8H24v8h-1.5zM0 8h1.5v8H0z"/><path fill="none" d="M7 17h10V7H7v10zm2.8-5l1.5 2 2.4-2.8 2.9 3.8h-9l2.2-3z"/><path d="M13.6 11.2L11.2 14l-1.4-2-2.3 3h9z"/><path d="M18 5H6c-.5 0-1 .5-1 1v12c0 .5.5 1 1 1h12c.5 0 1-.5 1-1V6c0-.5-.5-1-1-1zm-1 12H7V7h10v10z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>',
+  article: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M19 5v14H5V5h14m0-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-5 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"/><path fill="none" d="M0 0h24v24H0z"/></svg>',
+  help: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>',
+  speaker: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 14H5.17l-.59.59-.58.58V4h16v12zM6 12h2v2H6zm0-3h2v2H6zm0-3h2v2H6zm4 6h5v2h-5zm0-3h8v2h-8zm0-3h8v2h-8z"/><path fill="none" d="M0 0h24v24H0V0z"/></svg>'
 };
 
 function help(store) {
@@ -58,9 +58,9 @@ function buildHelp(store) {
     opts: ['<kbd>esc</kbd>']
   }];
 
-  if (store.state.presentation.request) {
+  if (store.state.presentation.channel) {
     keyboard.push({
-      desc: 'Toggle speaker notes/presentation view (requires external monitor)',
+      desc: 'Toggle speaker notes/presentation view',
       opts: ['<kbd>alt</kbd>+<kbd>shift</kbd>+<kbd>s</kbd>']
     });
   }
@@ -81,9 +81,9 @@ function buildHelp(store) {
     desc: 'Launch the help overlay.'
   }];
 
-  if (store.state.presentation.request) {
+  if (store.state.presentation.channel) {
     iconography.push({
-      desc: 'Toggle speaker notes/presentation view (requires external monitor)',
+      desc: 'Toggle speaker notes/presentation view',
       icon: icons.speaker
     });
   }
